87a7d1ec7878e6983a614a348dd83b56a0231839
steadyBOMBER <169099503+steadyBOMBER@users.noreply.github.com>
2025-10-17 11:07:18 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, Optional, Email
 
 class TrackForm(FlaskForm):
     tracking_number = StringField('Tracking Number', validators=[DataRequired(), Length(min=1, max=50)])
     email = StringField('Email', validators=[Optional(), Email(), Length(max=120)])
+    submit = SubmitField('Track')
