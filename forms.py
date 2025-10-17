6d2742b75d3703f808ce90ccbbddb97e72aafcf6
steadyBOMBER <169099503+steadyBOMBER@users.noreply.github.com>
2025-10-17 10:48:52 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+   from flask_wtf import FlaskForm
+   from wtforms import StringField
+   from wtforms.validators import DataRequired, Length
 
-class TrackForm(FlaskForm):
-    tracking_number = StringField('Tracking Number', validators=[DataRequired()])
+   class TrackForm(FlaskForm):
+       tracking_number = StringField('Tracking Number', validators=[DataRequired(), Length(min=1, max=50)])
